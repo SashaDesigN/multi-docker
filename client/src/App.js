2e4fb317f147e7a442fa01c2c603d54653a17570
root <SashaDesigN>
2019-01-30 21:05:45 +0200

    a small nav change

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
             <p>
               Edit <code>src/App.js</code> and save to reload.
             </p>
-            <Link to="/">Home</Link>
+            <Link to="/">Home Page</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
